7114a666b7eb4edb1977048667dd1f35f7913910
Rajat-Dub3y <raja1dub3y@gmail.com>
2025-06-07 03:26:36 +0530

    Fixed a major mistake

--- backend/src/server.js
+++ backend/src/server.js
@@ -36,14 +36,12 @@ app.use("/api/users",userRouter)
 app.use("/api/chat",chatRouter)
 
 if(process.env.NODE_ENV==="production"){
-  app.use(express.static(path.join(__dirname,"../frontend/build")))
+  app.use(express.static(path.join(__dirname,"../frontend/dist")))
 
   app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"../frontend","build","index.html"))
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
   })
 }
-
-
 
 app.listen(port,()=>{
     console.log("Server is running on port 5001")
